37d2ad612fd01d31227af16b93db2f7632b26423
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-02 21:29:35 +0100

    add Object

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const API_URL_ALL = 'https://restcountries.com/v3.1/all';
 
+let countries;
+
+
 fetch(API_URL_ALL)
-.then((res) => res.json())
-.then((res) => console.log(res)); //<== pobiera informacje
+  .then((res) => res.json())
+//.then((res) => console.log(res)); //<== pobiera informacje
+  .then((countriesRaw) => 
+    countriesRaw.map((country) => {
+      return { // tworzymy obiekt
+        capital: country.capital,
+        population: country.population,
+        name: country.name.common,
+        region: country.region,
+        flagsUrl: country.flags.png,
+      };
+    })
+  );
+
 
