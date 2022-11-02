2773d6ddcb80ab8fa17814cf01e9a6739105ef93
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-02 22:46:45 +0100

    add Css

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-import { renderCountriesList } from "./dom.utils.js";
-const API_URL_ALL = 'https://restcountries.com/v3.1/all';
+import { renderDashboard } from "./view-dashboard.js";
+import { renderDetail } from "./view-detail.js";
 
-let countries;
-
-
-fetch(API_URL_ALL)
-  .then((res) => res.json())
-  .then((countriesRaw) => {
-    countries = countriesRaw.map((country) => {
-      return { //< tworzymy obiekt który pobierze nam dane ze API 
-        capital: country.capital && country.capital[0], //<pierwszy element tablicy ale nie które nie mają stolicy i dlatego && country.capital 
-        population: country.population,
-        name: country.name.common,
-        region: country.region,
-        flagsUrl: country.flags.png,
-      };
-    });
-    renderCountriesList(countries);
-  });
+if (window.location.search.includes("?country=")) {
+    renderDetail();
+} else {
+    document.querySelector(".filters").classList.add("active");
+    renderDashboard();
+}
