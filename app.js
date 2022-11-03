3e1afd1483b1e6597f996503875cf802eb6087ef
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-03 20:14:18 +0100

    event bubbling example

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const trzy = document.querySelector('#trzy');
 console.log(trzy);
 
 jeden.addEventListener('click', () => {
+    console.log('klik w jeden');
     jeden.style['background-color'] = 'black';
 });
 
 dwa.addEventListener('click', () => {
+    console.log('klik w dwa');
     dwa.style['background-color'] = 'green';
 })
 
 trzy.addEventListener('click', () => {
+    console.log('klik w trzy');
     trzy.style['background-color'] = 'white';
 })
