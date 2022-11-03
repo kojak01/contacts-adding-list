8265b914fd855d326fe7b35fd968702ea708ed4c
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-11-03 19:40:04 +0100

    init-project

--- app.js
+++
@@ -1,9 +0,0 @@
-import { renderDashboard } from "./view-dashboard.js";
-import { renderDetail } from "./view-detail.js";
-
-if (window.location.search.includes("?country=")) {
-    renderDetail();
-} else {
-    document.querySelector(".filters").classList.add("active");
-    renderDashboard();
-}
